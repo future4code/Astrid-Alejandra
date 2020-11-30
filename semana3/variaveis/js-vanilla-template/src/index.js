76707593cae6f2fd6fd5c0f74bb8dffe27b12234
Astrid Bemporad <astridbemporad@gmail.com>
2020-11-30 16:55:55 -0300

    Exercicios de hoje!

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,26 @@
+/*
+Exercicio 1: 
+10 5
+
+Exercicio 2:
+10 10 10
+*/
+
+let nome = null
+let idade = null
+
+console.log = typeof(nome)
+console.log = typeof(idade)
+
+/* a resposta to typeof foi object.
+eu diria que o tipo é object 
+porque null implica uma variável que
+não tem valor ainda, mas vai ter!
+então o tipo object acaba sendo mais
+abrangente, e não restringe as 
+possibilidades. vi na internet que é
+um bug, mas não entendi muito bem.
+*/
+
+let nome = prompt("Digite seu nome!")
+let idade = prompt("Digite a sua idade!")
