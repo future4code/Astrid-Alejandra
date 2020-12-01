fdd965839b7d6c5f4365d0a87c6eed27939ea42f
Astrid Bemporad <astridbemporad@gmail.com>
2020-12-01 16:57:41 -0300

    Exercício de hoje

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,23 @@
+/* Exercício Interpretação 1
+a. false
+b. false
+c. true
+e. boolean
+*/
+
+/* Exercício Interpretação 2
+a. undefined
+b. EXECUTEI O PROGRAMA E VI QUE É 
+UNDEFINED, MAS NÃO ENTENDI POR QUÊ.
+acho que misturei na cabeça com o typeof
+bora pesquisar.
+c. 11
+d. 3
+e. 3 19 5 6 7 8 9 10 11 12 13
+f. 9
+*/
+
+//Exercício Escrita 1
+let suaIdade = null
+let idadeAmiga = null
++
