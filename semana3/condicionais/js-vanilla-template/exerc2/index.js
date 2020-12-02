ea17e0216ac1558e5db918d4531d3e5120ac743d
Astrid Bemporad <astridbemporad@gmail.com>
2020-12-02 17:02:53 -0300

    Exercício de hoje

---
+++ semana3/condicionais/js-vanilla-template/exerc2/index.js
@@ -0,0 +1,9 @@
+let array = [1, 2, 3, 4, 5, 6]
+
+console.log(array[0], array[2], array[5]) // 1 3 6
+
+array[0] = 2
+array[2] = 6
+array[4] = 10
+
+console.log(array)
---
+++ semana3/condicionais/js-vanilla-template/src/index.js
@@ -0,0 +1,20 @@
+// const souUmBooleano = true
+// console.log(typeof souUmBooleano)
+
+// const serieFavorita = "Brooklin 99"
+// console.log(typeof serieFavorita)
+
+// const temporadas = 7
+// console.log(typeof temporadas)
+
+
+// const personagens = ["Amy", "Jake", "Rosa"]
+// console.log(typeof personagens)
+
+let diretor
+console.log(diretor)
+console.log(typeof diretor)
+
+diretor = "Pedro"
+console.log(diretor)
+console.log(typeof diretor)
