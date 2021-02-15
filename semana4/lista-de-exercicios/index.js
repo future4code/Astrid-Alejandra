d69435bae5170376847e9316aaa92d97ca2d029f
Astrid Bemporad <astridbemporad@gmail.com>
2021-02-15 19:04:40 -0300

    Exercicios de interpretaçao

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,12 @@
+/*INTERPRETAÇÃO 1
+A função pega a cotaçao e o valor em dolar, retornando o valor em RS. Faz isso recebendo o valor em dolar como parámetro, e recebendo o valor da cotação que o usuário inseriu. No console vai aparecer o 100 vezes a cotação que o usuário inseriu. 
+*/
+/*INTERPRETAÇAO 2
+A função calcula o valor após investimento para vários tipos de investimento. Faz isso recebendo como parámetro o tipo de investimento e o valor do investimento, usando um switch case para avaliar todas as possíveis opções. Em cada case, vai calcular com os parámetros recebidos e uma razão fixa específica. No console vai ter 165 numa linha, e na seguinte vai dar undefined, e um alert: TIPO DE INVESTIMENTO INFORMADO INCORRETO!
+*/
+/*INTERPRETAÇÃ0 3
+A função vai separar os números do array números, deixando os números pares no array1 e os impares no array2. Faz isso usando um condicional if-else, e fazendo push em cada array de acordo com as condições estabelecidas. No console vai ter Quantidade total de numeros 14, na seguinte linha 6, e na seguinte 8.
+*/
+/*INTERPRETAÇÃO 4
+A funçao vai achar o maior número e o menor número do array números. Faz isso iterando por todos os items do array dois condicionais if, usando o for. No console vai imprimir -10 e na linha seguinte 1590.
+*/
