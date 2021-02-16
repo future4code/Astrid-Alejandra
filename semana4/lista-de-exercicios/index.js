65bc1bee11f4f158eb9f9867f7f014aa22280e6f
Astrid Bemporad <astridbemporad@gmail.com>
2021-02-16 18:51:11 -0300

    Exercicios de funçoes de array

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -172,3 +172,170 @@ function pessoaAnonima() {
 }
 console.log(pessoa);
 console.log(pessoaAnonima());
+//FUNCOES DE ARRAY 1
+const array3 = [
+  { nome: "Pedro", idade: 20 },
+  { nome: "João", idade: 10 },
+  { nome: "Paula", idade: 12 },
+  { nome: "Artur", idade: 89 },
+];
+//a)
+const filtrarAdultos = () => {
+  let newArray = [];
+  array3.filter((pessoa) => {
+    if (pessoa.idade >= 20) {
+      newArray = [...newArray, pessoa];
+    }
+  });
+  console.log(newArray);
+};
+filtrarAdultos();
+//b)
+const filtrarCriancas = () => {
+  let newArray = [];
+  array3.filter((pessoa) => {
+    if (pessoa.idade < 20) {
+      newArray.push(pessoa);
+    }
+  });
+  console.log(newArray);
+  return newArray;
+};
+filtrarCriancas();
+//FUNCOES DE ARRAY 2
+const array4 = [1, 2, 3, 4, 5, 6];
+//a)
+const vezesDois = () => {
+  const newArray = [];
+  array4.map((numero) => {
+    newArray.push(numero * 2);
+  });
+  console.log(newArray);
+};
+vezesDois();
+//b)
+const vezesTres = () => {
+  const newArray = [];
+  array4.map((numero) => {
+    let numeroString = 3 * numero.toString();
+    newArray.push(numeroString);
+  });
+  console.log(newArray);
+};
+vezesTres();
+//c)
+const parOuImpar = () => {
+  const newArray = [];
+  array4.map((numero) => {
+    if (numero % 2 === 0) {
+      newArray.push(`${numero} é par`);
+    } else {
+      newArray.push(`${numero} é impar`);
+    }
+  });
+  console.log(newArray);
+};
+parOuImpar();
+//FUNCOES DE ARRAY 3
+const pessoas = [
+  { nome: "Paula", idade: 12, altura: 1.8 },
+  { nome: "João", idade: 20, altura: 1.3 },
+  { nome: "Pedro", idade: 15, altura: 1.9 },
+  { nome: "Luciano", idade: 22, altura: 1.8 },
+  { nome: "Artur", idade: 10, altura: 1.2 },
+  { nome: "Soter", idade: 70, altura: 1.9 },
+];
+//a)
+const verificacaoPode = () => {
+  const newArray = [];
+  pessoas.filter((pessoa) => {
+    if (pessoa.altura >= 1.5 && pessoa.idade < 60 && pessoa.idade > 14) {
+      newArray.push(pessoa);
+    }
+  });
+  console.log(newArray);
+};
+verificacaoPode();
+//b)
+const verificacaoNaoPode = () => {
+  const newArray = [];
+  pessoas.filter((pessoa) => {
+    if (pessoa.altura < 1.5 || pessoa.idade > 60 || pessoa.idade < 14) {
+      newArray.push(pessoa);
+    }
+  });
+  console.log(newArray);
+};
+verificacaoNaoPode();
+//FUNCOES DE ARRAY 4
+const consultas = [
+  {
+    nome: "João",
+    genero: "masculino",
+    cancelada: true,
+    dataDaConsulta: "01/10/2019",
+  },
+  {
+    nome: "Pedro",
+    genero: "masculino",
+    cancelada: false,
+    dataDaConsulta: "02/10/2019",
+  },
+  {
+    nome: "Paula",
+    genero: "feminino",
+    cancelada: true,
+    dataDaConsulta: "03/11/2019",
+  },
+  {
+    nome: "Márcia",
+    genero: "feminino",
+    cancelada: false,
+    dataDaConsulta: "04/11/2019",
+  },
+];
+
+const emailConsulta = () => {
+  let tratamento = "";
+  let lembrete = "";
+  consultas.map((pessoa) => {
+    if (pessoa.genero === "masculino") {
+      tratamento = "Sr.";
+      lembrete = "lembrá-lo";
+    } else {
+      tratamento = "Sra.";
+      lembrete = "lembrá-la";
+    }
+    if (pessoa.cancelada) {
+      console.log(
+        `Olá, ${tratamento} ${pessoa.nome}. Infelizmente, sua consulta marcada para o dia ${pessoa.dataDaConsulta} foi cancelada. Se quiser, pode entrar em contato conosco para remarcá-la.`
+      );
+    } else {
+      console.log(`Olá, ${tratamento} ${pessoa.nome}. Estamos enviando esta mensagem para
+                ${lembrete} da sua consulta no dia ${pessoa.dataDaConsulta}. Por favor, acuse
+                o recebimento deste e-mail.`);
+    }
+  });
+};
+emailConsulta();
+//5
+const contas = [
+  { cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+  { cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+  { cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+  { cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+  { cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+  { cliente: "Soter", saldoTotal: 1200, compras: [] },
+];
+
+const atualizaSaldo = () => {
+  contas.forEach((conta) => {
+    let totalGastos = 0;
+    conta.compras.forEach((valor) => {
+      totalGastos += valor;
+    });
+    conta.saldoTotal -= totalGastos;
+  });
+  console.log(contas);
+};
+atualizaSaldo();
