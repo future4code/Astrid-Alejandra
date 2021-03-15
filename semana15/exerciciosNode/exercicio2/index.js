99bcfde8bcc810734842d7fff45bc31a06a36b5d
astrid <astridbemporad@gmail.com>
2021-03-15 19:00:45 -0300

    Exercicio 4, desafios 1 e 2

--- semana15/exerciciosNode/exercicio2/index.js
+++ semana15/exerciciosNode/exercicio2/index.js
@@ -1,16 +1,18 @@
+const colors = require("colors");
+
 const operation = process.argv[2];
 
 const num1 = Number(process.argv[3]);
 const num2 = Number(process.argv[4]);
 
-const addition = () => console.log(`Answer: ${num1 + num2}`);
-const subtraction = () => console.log(`Answer: ${num1 - num2}`);
-const multiplication = () => console.log(`Answer: ${num1 * num2}`);
+const addition = () => console.log(`Answer: ${num1 + num2}`.red);
+const subtraction = () => console.log(`Answer: ${num1 - num2}`.blue);
+const multiplication = () => console.log(`Answer: ${num1 * num2}`.green);
 const division = () => {
   if (num2 !== 0) {
-    console.log(`Answer: ${num1 / num2}`);
+    console.log(`Answer: ${num1 / num2}`.magenta);
   } else {
-    console.log("Ooops, division by zero is not defined :(");
+    console.log("Ooops, division by zero is not defined :(".cyan);
   }
 };
 
@@ -32,8 +34,8 @@ if (operation) {
       console.log("");
   }
 } else {
-  console.log("Please choose a valid operation: add, subt, mult or div");
+  console.log("Please choose a valid operation: add, subt, mult or div".grey);
 }
 if (!num1 || !num2) {
-  console.log("Please, enter two numbers to get an answer");
+  console.log("Please, enter two numbers to get an answer".yellow);
 }
