5ee51c47392ae1d21cb4391bd038911460b94148
astrid <astridbemporad@gmail.com>
2021-04-11 10:48:25 -0300

    Create project all over again from zero, create getAllUsers and createUser endpoints

--- semana16/toDoList/src/endpoints/user/createUser.ts
+++ semana16/toDoList/src/endpoints/user/createUser.ts
@@ -2,30 +2,52 @@ import { Request, Response } from "express";
 import connection from "../../connection";
 import { User } from "../../types/types";
 
-const createUser = async (req: Request, res: Response) => {
+const createUser = async (req: Request, res: Response): Promise<void> => {
   try {
-    const { id, name, nickname, email } = req.body;
+    const { name, nickname, email } = req.body;
     const userData: User = {
-      id: id,
       name: name,
       nickname: nickname,
       email: email,
     };
-    const newUser = await 
+    if (!name || !nickname || !email.includes("@")) {
+      res.statusCode = 400;
+      throw new Error("All fields are required");
+    }
+
+    const verifiedEmail = await verifyEmail(email);
+    if (!verifiedEmail) {
+      res.statusCode = 409;
+      throw new Error(`${email} is already being used`);
+    } else {
+      const newUser = await insertUser(userData);
+      res.send(newUser);
+    }
   } catch (error) {
     if (res.statusCode === 200) {
-      res.status(500).send({ message: "Internal server error" });
+      res.status(500).send("Internal Server Error");
     } else {
       res.send({ message: error.message });
     }
   }
 };
 
-const insertUser = async (userData: User) => {
-  const result = await connection("TDLUser")
-  .insert(userData);
+const insertUser = async (userData: User): Promise<number[]> => {
+  const result = await connection("TDLUser").insert(userData);
 
-    return result
+  return result;
+};
+
+const verifyEmail = async (reqEmail: string): Promise<boolean> => {
+  const result = await connection("TDLUser")
+    .select("*")
+    .where({ email: reqEmail });
+
+  if (result.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 export default createUser;
