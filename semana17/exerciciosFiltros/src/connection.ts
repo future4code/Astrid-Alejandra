8f293798dc0f13027d28def1c50ac262588191c7
astrid <astridbemporad@gmail.com>
2021-04-12 20:25:02 -0300

    Create endpoints of exercicio 2

--- semana17/exerciciosFiltros/src/connection.ts
+++ semana17/exerciciosFiltros/src/connection.ts
@@ -1,5 +1,5 @@
-import dotenv from "dotenv";
 import knex from "knex";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -7,10 +7,11 @@ const connection = knex({
   client: "mysql",
   connection: {
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || "3306"),
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: 3306,
+    multipleStatements: true,
   },
 });
 
--- semana17/exerciciosFiltros/src/index.ts
+++ semana17/exerciciosFiltros/src/index.ts
@@ -1,1 +1,10 @@
 import app from "./app";
+import { getAllUsers } from "./endpoints/getAllUsers";
+import getUserByName from "./endpoints/getUserByName";
+import getUserByType from "./endpoints/getUserByType";
+import getUsersOrderedBy from "./endpoints/getUsersOrderedBy";
+
+app.get("/users/all", getAllUsers);
+app.get("/users/name", getUserByName);
+app.get("/users/search", getUsersOrderedBy);
+app.get("/users/search/:type", getUserByType);
