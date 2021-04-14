dbc4073e6930d7b9d7b6150e4a4cce27b71df05c
astrid <astridbemporad@gmail.com>
2021-04-14 16:21:42 -0300

    Refining some imports

--- semana18/exerciciosAutenticacao/src/index.ts
+++ semana18/exerciciosAutenticacao/src/index.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import editUser from "./endpoints/editUser";
 import getAllUsers from "./endpoints/getAllUsers";
 import getUserById from "./endpoints/getUserById";
 import logIn from "./endpoints/logIn";
@@ -11,6 +10,5 @@ index.get("/users", getAllUsers);
 index.get("/users/profile", getUserById);
 index.post("/users/signup", signUp);
 index.post("/users/login", logIn);
-index.put("/users/edit", editUser);
 
 export default index;
