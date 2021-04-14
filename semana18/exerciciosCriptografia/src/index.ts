54630290230be5b26c8d219db1f7476be6b55d83
astrid <astridbemporad@gmail.com>
2021-04-14 17:20:10 -0300

    Create deleteUserById, as specified

--- semana18/exerciciosCriptografia/src/index.ts
+++ semana18/exerciciosCriptografia/src/index.ts
@@ -3,6 +3,7 @@ import getUserById from "./endpoints/getUserById";
 import getAllUsers from "./endpoints/getAllUsers";
 import logIn from "./endpoints/logIn";
 import signUp from "./endpoints/signUp";
+import deleteUserById from "./endpoints/deleteUserById";
 
 const index = Router();
 
@@ -10,5 +11,6 @@ index.get("/users", getAllUsers);
 index.get("/users/profile", getUserById);
 index.post("/users/signup", signUp);
 index.post("/users/login", logIn);
+index.delete("/users/:id", deleteUserById);
 
 export default index;
