8fd13c60fbd42b798bf91e74b13b23bf7b52b879
astrid <astridbemporad@gmail.com>
2021-04-14 16:48:11 -0300

    Refactor getUserById endpoint

--- semana18/exerciciosCriptografia/src/index.ts
+++ semana18/exerciciosCriptografia/src/index.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import editUser from "./endpoints/editUser";
+import getUserById from "./endpoints/getUserById";
 import getAllUsers from "./endpoints/getAllUsers";
 import logIn from "./endpoints/logIn";
 import signUp from "./endpoints/signUp";
@@ -7,8 +7,8 @@ import signUp from "./endpoints/signUp";
 const index = Router();
 
 index.get("/users", getAllUsers);
+index.get("/users/profile", getUserById);
 index.post("/users/signup", signUp);
 index.post("/users/login", logIn);
-index.put("/users/edit", editUser);
 
 export default index;
